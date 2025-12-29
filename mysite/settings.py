96139c2fcdf1805244b8fef0ae76038dc70743a0
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2025-12-28 18:44:16 -0600

    routes

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%0=opji4dnet+6imyw25wp^rg0@nm@%un1yf#7s(ifp0b0ale3'
+SECRET_KEY = 'django-insecure-%92+ga!&j(-w9k^4h3#wbqq!a5%uzr6_3pvt71@x_3aa5@b%o)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
